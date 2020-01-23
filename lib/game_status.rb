a82aea3929765fca1488056bdb7036be73955806
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-23 22:26:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2], #first horizontal WIN_COMBINATION
+  [0, 1, 2], #first horizontal WIN_COMBINATION 
   [3, 4, 5], #second 
   [6, 7, 8], #third 
   [0, 3, 6], #first vertical WIN_COMBINATION
@@ -17,18 +17,18 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combination| #creates new variable to iterate through the constant
-    index_1 = win_combination[0] #grabs the index of a winning combination and stores in new array
-    index_2 = win_combination[1]
-    index_3 = win_combination[2]
+    index_1 = win_combination[0] #grabs the index of a winning combination at index of each ele
+    index_2 = win_combination[1] #([0,1,2]) = index of each element in the constant
+    index_3 = win_combination[2] #index stores position of winning combinations 
 
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
+    position_1 = board[index_1] #loads value of the board at the index 1 
+    position_2 = board[index_2] #loads value of the board at the index 2
+    position_3 = board[index_3] #loads value of the board at the index 3
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" #checks each board position and                                                              #and compares to winning index
+      return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+      return win_combination
     end
   }
   return false
